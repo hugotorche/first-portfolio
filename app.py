24cbfee7bb5e7d4a83a739a519536ab7b4b72868
Hugo Torche <hugo.torche@kedgebs.com>
2025-08-22 12:06:46 +0200

    update app host for fly io

--- app.py
+++ app.py
@@ -126,4 +126,4 @@ def get_static_json(path):
 
 if __name__ == "__main__":
     print("running py app")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080)
